938acecd646e82c37bd00a3cf2221101e21c5dd3
6395728950 <161918761+6395728950@users.noreply.github.com>
2025-05-09 18:49:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
 //your JS code here. If required.
+
+const array = [1,2,3,4];
+ const div = document.getElementById('output');
+
+const promise1 = () => {
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve(array);
+		},3000);
+	});
+};
+ 
+ 
+const promise2 = () => {
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			const arr = array.filter(num=>num%2===0);
+			resolve(arr);
+		},1000);
+	});
+};
+  
+const promise3 = () => {
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			const updatedArr=array.filter(num=>num%2===0).map(num=>num*2);
+			resolve(updatedArr);
+		},2000);
+		 
+	});
+};
+
+promise1().then((array)=>{
+ 
+div.innerHTML=array;
+	 return promise2();
+}).then((arr)=>{
+	div.innerHTML=arr;
+	 
+	return promise3();
+}).then((updatedArr)=>{
+	 
+	 
+	div.innerHTML=updatedArr;
+}).catch((err)=>{
+	const p = document.createElement('p');
+	p.textContent=err;
+	div.appendChild(p);
+})
+
+	
